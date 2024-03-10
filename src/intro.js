42ef7a296509b664b4a5a33e5ecff4445aad79eb
glckfndr <obulan65@gmail.com>
2024-03-10 18:50:58 +0200

    test: add intro test and implementations

--- src/intro.js
+++ src/intro.js
@@ -12,3 +12,17 @@ export function fizzBuzz(n) {
   if (n % 5 === 0) return 'Buzz';
   return n.toString();
 }
+
+export function calculateAverage(arr){
+
+  if(arr.length >= 1){
+    return arr.reduce((acc, el) => acc + el, 0)/arr.length;
+  }
+  return NaN;
+}
+
+export function factorial(n){
+  if(n < 0) return undefined;
+  if(n <= 1) return 1;
+  return n * factorial(n - 1)
+}
